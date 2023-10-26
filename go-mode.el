e7ab779082eb195088d991bf61cb0f50ee5203b4
Jason Bury <jason.bury@flexe.com>
2023-10-26 14:59:42 -0700

    Initial config, largely repurposed from JR's

---
+++ go-mode.el
@@ -0,0 +1,79 @@
+;;; +go-mode.el -*- lexical-binding: t; -*-
+
+(map! :after go-ts-mode
+      :map go-ts-mode-map
+      :localleader
+      "a" #'go-tag-add
+      "d" #'go-tag-remove
+      "e" #'+go/play-buffer-or-region
+      "i" #'go-goto-imports      ; Go to imports
+      (:prefix ("h" . "help")
+               "." #'godoc-at-point     ; Lookup in godoc
+               "d" #'go-guru-describe   ; Describe this
+               "v" #'go-guru-freevars   ; List free variables
+               "i" #'go-guru-implements ; Implements relations for package types
+               "p" #'go-guru-peers      ; List peers for channel
+               "P" #'go-guru-pointsto   ; What does this point to
+               "r" #'go-guru-referrers  ; List references to object
+               "e" #'go-guru-whicherrs  ; Which errors
+               "w" #'go-guru-what       ; What query
+               "c" #'go-guru-callers    ; Show callers of this function
+               "C" #'go-guru-callees)   ; Show callees of this function
+      (:prefix ("ri" . "imports")
+               "a" #'go-import-add)
+      (:prefix ("b" . "build")
+       :desc "go run ." "r" (cmd! (compile "go run ."))
+       :desc "go build" "b" (cmd! (compile "go build"))
+       :desc "go clean" "c" (cmd! (compile "go clean")))
+      (:prefix ("t" . "test")
+               "t" #'+go/test-rerun
+               "a" #'+go/test-all
+               "s" #'+go/test-single
+               "n" #'+go/test-nested
+               "f" #'+go/test-file
+               "g" #'go-gen-test-dwim
+               "G" #'go-gen-test-all
+               "e" #'go-gen-test-exported
+               (:prefix ("b" . "bench")
+                        "s" #'+go/bench-single
+                        "a" #'+go/bench-all)))
+
+(after! go-mod-ts-mode
+  (add-hook 'go-mod-ts-mode-hook #'lsp!))
+
+(after! go-ts-mode
+  (add-hook 'go-ts-mode-hook #'lsp!)
+  (setq! treesit-font-lock-level 4
+         go-ts-mode-indent-offset 4) ;; angry fruit salad
+  (set-docsets! 'go-ts-mode "Go")
+  (set-repl-handler! 'go-ts-mode #'gorepl-run)
+  (set-lookup-handlers! 'go-ts-mode
+    :definition #'go-guru-definition
+    :references #'go-guru-referrers
+    :documentation #'godoc-at-point))
+
+(after! (:or go-mode go-ts-mode)
+  (add-hook 'go-mode-hook #'format-all-mode)
+  (setq gofmt-command 'goimports)
+
+  ;; I use advice for the below because there are a couple of other commands
+  ;; that utilize the advised commands and they work just fine
+  ;; This is a little dumb
+  (defadvice! +ginkgo--+go--run-tests-a (args)
+    :override #'+go--run-tests
+    (+ginkgo--+go--run-tests args))
+
+  (defadvice! +ginkgo--+go/test-all-a ()
+    :override #'+go/test-all
+    (+ginkgo--+go/test-all))
+
+  (defadvice! +ginkgo--+go/test-file-a ()
+    :override #'+go/test-file
+    (+ginkgo--+go/test-file))
+
+  (defadvice! +ginkgo--+go/test-single-a ()
+    :override #'+go/test-single
+    (+ginkgo--+go/test-single)))
+
+;; (setq flycheck-golangci-lint-deadline "10m"
+;;       flycheck-golangci-lint-fast t)
---
+++ lsp-mode.el
@@ -0,0 +1,51 @@
+;;; +lsp-mode.el -*- lexical-binding: t; -*-
+
+(after! yaml-ts-mode
+  (add-hook 'yaml-ts-mode-hook #'lsp!))
+
+(after! lsp-mode
+;  (setq! read-process-output-max (* 1024 1024)
+;         company-idle-delay 1
+;         company-minimum-prefix-length 3)
+
+  (setq! +lsp-company-backends 'company-capf)
+
+  (setq! lsp-log-io nil
+         lsp-enable-suggest-server-download nil
+         lsp-enable-symbol-highlighting nil
+         lsp-response-timeout 120
+         lsp-modeline-diagnostics-scope :workspace
+         lsp-terraform-ls-enable-show-reference t
+         lsp-terraform-ls-prefill-required-fields t
+         lsp-terraform-ls-validate-on-save t)
+
+  (setq! lsp-go-goimports-local "gitlab.com/flexe"
+         lsp-go-use-gofumpt t
+         lsp-go-use-placeholders nil
+         lsp-diagnostics-provider :auto)
+
+  ;; :json-false
+  (setq! lsp-go-codelenses '((gc_details . t) (generate . t) (regenerate_cgo . t) (run_govulncheck . t)
+                             (test . t) (tidy . t) (upgrade_dependency . t) (vendor . t))
+
+         lsp-go-analyses '((appends . t) (asmdecl . t) (assign . t) (atomic . t) (atomicalign . t) (bools . t)
+                           (buildtag . t) (cgocall . t) (composites . t) (copylocks . t) (deepequalerrors . t) (defers . t)
+                           (deprecated . t) (embed . t) (fieldalignment . t) (httpresponse . t) (ifaceassert . t)
+                           (loopclosure . t) (lostcancel . t) (nilness . t) (printf . t) (shadow . t) (shift . t)
+                           (simplifycompositelit . t) (simplifyrange . t) (simplifyslice . t) (slog . t) (sortslice . t)
+                           (stdmethods . t) (stringintconv . t) (structtag . t) (testinggoroutine . t) (tests . t) (timeformat . t)
+                           (unmarshal . t) (unreachable . t) (unsafeptr . t) (unusedparams . t) (unusedresult . t) (unusedwrite . t)
+                           (useany . t) (fillreturns . t) (nonewvars . t) (undeclaredname . t) (unusedvariable . t) (fillstruct . t)
+                           (infertypeargs . t) (stubmethods . t)))
+
+  (setq! lsp-go-directory-filters ["-**/node_modules" "-**/.direnv" "-**/.devenv"])
+
+  (lsp-register-custom-settings
+   '(("gopls.completeUnimported" t t)
+     ("gopls.staticcheck" t t)))
+
+  (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.devenv\\'"))
+
+
+(after! (:and lsp-mode treemacs-mode lsp-treemacs)
+  (lsp-treemacs-sync-mode 1))
